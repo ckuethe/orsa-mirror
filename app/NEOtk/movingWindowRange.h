5b4b35d37e0837a714c3c3051f832874a70c2aa4
Pasquale Tricarico <tricaric@users.sourceforge.net>
2011-03-08 19:50:22 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

---
+++ app/NEOtk/movingWindowRange.h
@@ -0,0 +1,16 @@
+#ifndef _MOVING_WINDOW_RANGE_H_
+#define _MOVING_WINDOW_RANGE_H_
+
+#include <osg/Referenced>
+#include <osg/ref_ptr>
+
+class MovingWindowRange : public osg::Referenced {
+public:
+    MovinWindowRange() : osg::Referenced() {
+        
+    }
+protected:
+    ~MovingWindowRange() { }
+};
+
+#endif // _MOVING_WINDOW_RANGE_H_
