d5d2a341e4ff1a430ea2ca7350331dd179a35a69
Pasquale Tricarico <tricaric@users.sourceforge.net>
2011-03-17 02:57:43 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- src/orsaPDS/RadioScienceGravity.cpp
+++ src/orsaPDS/RadioScienceGravity.cpp
@@ -3,11 +3,101 @@
 #include <iostream>
 #include <cstdio>
 
+#include <orsa/print.h>
+
 using namespace orsaPDS;
 
-RadioScienceGravityFile::RadioScienceGravityFile(const std::string & fileName) {
+RadioScienceGravityFile::RadioScienceGravityFile(const std::string & fileName,
+                                                 const size_t & RECORD_BYTES_,
+                                                 const size_t & FILE_RECORDS_) :
+    RECORD_BYTES(RECORD_BYTES_),
+    FILE_RECORDS(FILE_RECORDS) {
+    
     data = new RadioScienceGravityData;
-    FILE * fp = fopen(fileName.c_str(),"r");
-    if (!fp) return;
+    
+    fp = fopen(fileName.c_str(),"r");
+    if (!fp) {
+        ORSA_DEBUG("cannot open file [%s]",fileName.c_str());
+        return;
+    }
+    
+    // read SHBDR_HEADER_TABLE
+    {
+        readD(data->R0);
+        orsa::FromUnits(data->R0,orsa::Unit::KM);
+        
+        readD(data->GM);
+        orsa::FromUnits(orsa::FromUnits(data->GM,orsa::Unit::KM,3),orsa::Unit::SECOND,-2);
+        
+        readD(data->sigmaGM);
+        orsa::FromUnits(orsa::FromUnits(data->sigmaGM,orsa::Unit::KM,3),orsa::Unit::SECOND,-2);
+        
+        readI(data->degree);
+
+        readI(data->order);
+
+        readI(data->normalizationState);
+
+        readI(data->numberOfCoefficients);
+        
+        readD(data->referenceLongitude);
+        data->referenceLongitude *= orsa::degToRad();
+        
+        readD(data->referenceLatitude);
+        data->referenceLatitude *= orsa::degToRad();
+        
+        skipToNextRow();
+    }
+
+    ORSA_DEBUG("n. coeff: %i",data->numberOfCoefficients);
+    
+    // read SHBDR_NAMES_TABLE
+    {
+        for (unsigned int k=0; k<data->numberOfCoefficients; ++k) {
+            std::string s;
+            readS(s);
+            ORSA_DEBUG("name: [%s]",s.c_str());
+        }
+        
+        skipToNextRow();
+    }
+    
     fclose(fp);
+}
+
+
+bool RadioScienceGravityFile::readD(double & d) const {
+    const bool retVal = (1 == fread(&d,sizeof(double),1,fp));
+    if (!retVal) {
+        ORSA_DEBUG("problems...");
+    }
+    return retVal;
+}
+
+bool RadioScienceGravityFile::readI(int & i) const {
+    const bool retVal = (1 == fread(&i,sizeof(int),1,fp));
+    if (!retVal) {
+        ORSA_DEBUG("problems...");
+    }
+    return retVal;
+}
+
+bool RadioScienceGravityFile::readS(std::string & s) const {
+    char line[8];
+    const bool retVal = (8 == fread(&line,sizeof(char),8,fp));
+    if (!retVal) {
+        ORSA_DEBUG("problems...");
+    }
+    s = line;
+    return retVal;
+}
+
+void RadioScienceGravityFile::skipToNextRow() const {
+    long pos = ftell(fp);
+    ORSA_DEBUG("pos before seek: %i",pos);
+    if ((pos%RECORD_BYTES) != 0) {
+        fseek(fp,RECORD_BYTES-(pos%RECORD_BYTES),SEEK_CUR);
+    }
+    pos = ftell(fp);
+    ORSA_DEBUG("pos after seek: %i",ftell(fp));
 }
--- src/orsaPDS/RadioScienceGravity.h
+++ src/orsaPDS/RadioScienceGravity.h
@@ -9,16 +9,37 @@
 namespace orsaPDS {
     
     class RadioScienceGravityData : public osg::Referenced {
-        
+    public:
+        double R0, GM, sigmaGM;
+        int degree, order;
+        int normalizationState;
+        int numberOfCoefficients;
+        double referenceLongitude, referenceLatitude;
     };
     
     class RadioScienceGravityFile : public osg::Referenced {
     public:
-        RadioScienceGravityFile(const std::string & fileName);
+        // read RECORD_BYTES and FILE_RECORDS from the label of the PDS file
+        RadioScienceGravityFile(const std::string & fileName,
+                                const size_t & RECORD_BYTES_,
+                                const size_t & FILE_RECORDS_);
     public:
         const RadioScienceGravityData * getData() const { return data.get(); }
     protected:
         osg::ref_ptr<RadioScienceGravityData> data;
+    protected:
+        bool readD(double & d) const;
+        bool readI(int & i) const;
+        bool readS(std::string & s) const;
+    protected:
+        void skipToNextRow() const;
+        
+    protected:
+        const size_t RECORD_BYTES;
+        const size_t FILE_RECORDS;
+        
+    protected:
+        FILE * fp;
     };
     
 } // namespace orsaPDS
