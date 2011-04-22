513ea0efce74223dc646b2342f023c407301037d
Pasquale Tricarico <tricaric@users.sourceforge.net>
2011-04-22 03:51:34 -0700

    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- src/orsaUtil/adaptiveMonteCarlo.h
+++ src/orsaUtil/adaptiveMonteCarlo.h
@@ -20,16 +20,39 @@ namespace orsaUtil {
     protected:
         virtual ~AdaptiveMonteCarlo() { }
     public:
-        virtual bool run(std::vector< osg::ref_ptr<orsaUtil::AdaptiveInterval<T> > > & intervalVector,
+        // two different integers between 0 and N-1, with z1 < z2
+        static bool z1z2(size_t & z1, size_t & z2, const size_t & N) {
+            if (N<2) return false;
+            // don't use ___z1 and ___z2 in code below, but z1 and z2 
+            size_t ___z1 = orsa::GlobalRNG::instance()->rng()->gsl_rng_uniform_int(N);
+            size_t ___z2;
+            do { ___z2 = orsa::GlobalRNG::instance()->rng()->gsl_rng_uniform_int(N); }
+            while (___z1 == ___z2);
+            // save them as z1 < z2 to ensure that vectors...[z1] are set before vectors...[z2]
+            z1 = std::min(___z1,___z2);
+            z2 = std::max(___z1,___z2);
+            return true;
+        }
+    public:
+        virtual bool run(T & intervalVector,
                          const size_t & maxIter) {
+            N=intervalVector.size();
+            N.lock();
             doAbort=false;
-            size.lock();
             size_t iter=0;
+            unsigned int ___z1=0, ___z2=0; // init once to make compiler happy
             while (iter<maxIter) {
                 ++iter;
+                if (doAbort) break;
+                
+                // don't use ___z1 and ___z2 in code below, but z1 and z2
+                if (!z1z2(___z1,___z2,N)) { ORSA_DEBUG("problems..."); }
+                const size_t z1 = ___z1; const size_t z2 = ___z2;
+                ORSA_DEBUG("z1: %i   z2: %i",z1,z2);
+                
                 
             }
-            size.unlock();
+            N.unlock();
             return true;
         }
     protected:
@@ -39,7 +62,9 @@ namespace orsaUtil {
     private:
         mutable bool doAbort;
     public:
-        orsa::Cache<size_t> size;
+        orsa::Cache<size_t> N;
+    protected:
+        // osg::ref_ptr<orsa::RNG> rng;
     };
     
 }; // namespace orsaUtil
