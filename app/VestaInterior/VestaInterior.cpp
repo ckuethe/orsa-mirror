1514b9fe2fa848e94d5c573ab743b6cc632ad482
Pasquale Tricarico <tricaric@users.sourceforge.net>
2011-07-03 04:14:19 -0700

    Testing cubic Chebyshev mass distribution.
    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- app/VestaInterior/VestaInterior.cpp
+++ app/VestaInterior/VestaInterior.cpp
@@ -2,7 +2,43 @@
 
 int main() {
     
+    const size_t order = 20;
+    CubicChebyshevMassDistribution::CoefficientType coeff;
+    coeff.resize(order+1);
+    for (size_t i=0; i<=order; ++i) {
+        coeff[i].resize(order+1-i);
+        for (size_t j=0; j<=order-i; ++j) {
+            coeff[i][j].resize(order+1-i-j);
+        }
+    }
     
+    // reset
+    for (size_t i=0; i<=order; ++i) {
+        for (size_t j=0; j<=order-i; ++j) {
+            for (size_t k=0; k<=order-i-j; ++k) {
+                coeff[i][j][k] = 0.0;
+            }
+        }
+    }
+
+    // set values
+    coeff[0][0][0] = 1.0;
+    coeff[0][0][1] = 0.1;
+    coeff[0][1][1] = 0.2;
+    coeff[2][1][3] = 0.2;
+    
+    osg::ref_ptr<CubicChebyshevMassDistribution> massDistribution =
+        new CubicChebyshevMassDistribution(coeff);
+
+    {
+        double x,y,z;
+        for (size_t s=0; s<1000000; ++s) {
+            orsa::GlobalRNG::instance()->rng()->gsl_ran_dir_3d(&x,&y,&z);
+            const orsa::Vector p(x,y,z);
+            const double density = massDistribution->density(p);
+            ORSA_DEBUG("SAMPLE %+9.6f %+9.6f %+9.6f %+9.6f",x,y,z,density);
+        }
+    }
     
     return 0;
 }
--- app/VestaInterior/VestaInterior.h
+++ app/VestaInterior/VestaInterior.h
@@ -1,13 +1,41 @@
 #ifndef _VESTA_INTERIOR_H_
 #define _VESTA_INTERIOR_H_
 
+#include <orsa/chebyshev.h>
+#include <orsa/massDistribution.h>
+
 #include <orsaUtil/adaptiveInterval.h>
 #include <orsaUtil/adaptiveMonteCarlo.h>
 
 // Interior model part
 
-
-
+class CubicChebyshevMassDistribution : public orsa::MassDistribution {
+public:
+    typedef std::vector< std::vector< std::vector<double> > > CoefficientType;
+protected:
+    CoefficientType coeff;
+public:
+    CubicChebyshevMassDistribution(const CoefficientType & coefficient) : orsa::MassDistribution(), coeff(coefficient) { }
+protected:
+    ~CubicChebyshevMassDistribution() { }
+public:
+    double density(const orsa::Vector & p) const {
+        const size_t order = coeff.size()-1;
+        double density = 0.0;
+        std::vector<double> Tx, Ty, Tz;
+        orsa::ChebyshevT(Tx,order,p.getX());
+        orsa::ChebyshevT(Ty,order,p.getY());
+        orsa::ChebyshevT(Tz,order,p.getZ());
+        for (size_t i=0; i<=order; ++i) {
+            for (size_t j=0; j<=order-i; ++j) {
+                for (size_t k=0; k<=order-i-j; ++k) {
+                    density += coeff[i][j][k]*Tx[i]*Ty[j]*Tz[k];
+                }
+            }
+        }
+        return density;
+    }
+};
 
 // AdaptiveInterval part
 
