137ea2850cadb7377cc6f8ee9ce54961567b7481
Pasquale Tricarico <tricaric@users.sourceforge.net>
2012-06-11 23:49:16 -0700

    Updated file format.
    Signed-off-by: Pasquale Tricarico <tricaric@users.sourceforge.net>

--- src/orsaInputOutput/JPL_comet.cpp
+++ src/orsaInputOutput/JPL_comet.cpp
@@ -26,22 +26,22 @@ bool JPLCometFile::processLine(const char * line) {
     std::string s_q, s_e, s_i, s_peri, s_node;
     std::string s_Tp_y, s_Tp_m, s_Tp_d;
   
-    s_designation.assign(line,0,38);
+    s_designation.assign(line,0,43);
     removeLeadingAndTrailingSpaces(s_designation);
   
     // ORSA_DEBUG("des: %s",s_designation.c_str());
   
-    s_epoch_MJD.assign(line,39,7);
+    s_epoch_MJD.assign(line,44,7);
   
-    s_q.assign(line,47,11);
-    s_e.assign(line,59,10);
-    s_i.assign(line,70,9);
-    s_peri.assign(line,80,9);
-    s_node.assign(line,90,9);
+    s_q.assign(line,52,11);
+    s_e.assign(line,64,10);
+    s_i.assign(line,75,9);
+    s_peri.assign(line,85,9);
+    s_node.assign(line,95,9);
   
-    s_Tp_y.assign(line,100,4);
-    s_Tp_m.assign(line,104,2);
-    s_Tp_d.assign(line,106,8);
+    s_Tp_y.assign(line,105,4);
+    s_Tp_m.assign(line,109,2);
+    s_Tp_d.assign(line,111,8);
   
     const Time Tp = orsaSolarSystem::gregorTime(atoi(s_Tp_y.c_str()),
                                                 atoi(s_Tp_m.c_str()),
